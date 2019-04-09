2d32f132f11bbfd5016569763d1b5a8f01aad105
Stefan Pratter <stefan@20c.com>
2019-04-09 09:05:22 +0000

    enable basic auth for ixf import preview (#408)

--- peeringdb_server/import_views.py
+++ peeringdb_server/import_views.py
@@ -1,8 +1,10 @@
 import json
+import base64
 
 from django.http import JsonResponse, HttpResponse
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import authenticate, login
 
 from django_namespace_perms.util import has_perms
 from ratelimit.decorators import ratelimit, is_ratelimited
@@ -12,8 +14,23 @@ from peeringdb_server.models import IXLan
 
 RATELIMITS = settings.RATELIMITS
 
+def enable_basic_auth(fn):
+    """
+    a simple decorator to enable basic auth for a specific view
+    """
+    def wrapped(request, *args, **kwargs):
+        if 'HTTP_AUTHORIZATION' in request.META:
+            auth = request.META['HTTP_AUTHORIZATION'].split()
+            if len(auth) == 2:
+                if auth[0].lower() == "basic":
+                    username, password = base64.b64decode(auth[1]).split(':', 1)
+                    login(request, authenticate(username=username, password=password))
+        return fn(request, *args, **kwargs)
+    return wrapped
+
 
 @ratelimit(key="ip", rate=RATELIMITS["view_import_ixlan_ixf_preview"])
+@enable_basic_auth
 def view_import_ixlan_ixf_preview(request, ixlan_id):
 
     # check if request was blocked by rate limiting
