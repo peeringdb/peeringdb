3c77cf0fc5be19470888b4bd3f3aba721e7f9355
egfrank <elliot.g.frank@gmail.com>
2020-07-06 12:29:59 -0500

    Remove validator now that constraint exists

--- peeringdb_server/serializers.py
+++ peeringdb_server/serializers.py
@@ -1361,9 +1361,6 @@ class NetworkIXLanSerializer(ModelSerializer):
             SoftRequiredValidator(
                 fields=("ipaddr4", "ipaddr6"), message="Input required for IPv4 or IPv6"
             ),
-            UniqueFieldValidator(
-                fields=("ipaddr4", "ipaddr6"), message="IP already exists"
-            ),
         ]
 
         model = NetworkIXLan
