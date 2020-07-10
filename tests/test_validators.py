b68852560b6a334b4d26fc44fef38b94b27f3175
Stefan Pratter <stefan@20c.com>
2020-07-10 08:21:09 +0000

    test for protected entities (#697)

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -5,6 +5,7 @@ import requests
 
 from django.test import override_settings
 from django.core.exceptions import ValidationError
+from django.core.management import call_command
 
 from peeringdb_server.validators import (
     validate_address_space,
@@ -22,6 +23,8 @@ from peeringdb_server.models import (
     IXLanPrefix,
     Network,
     NetworkContact,
+    Facility,
+    ProtectedAction,
 )
 
 pytestmark = pytest.mark.django_db
@@ -259,3 +262,133 @@ def test_validate_phonenumber():
 
     ix.policy_phone = "+1 206 555 0199"
     ix.full_clean()
+
+
+
+@pytest.mark.djangodb
+def test_protected_entities():
+    """
+    test that protected entities cannot be deleted
+    if their protection conditions are met
+    """
+
+    call_command("pdb_generate_test_data", limit=2, commit=True)
+
+    org = Organization.objects.first()
+
+    # assert that the org as active objects under it
+
+    assert org.ix_set_active.exists()
+    assert org.fac_set_active.exists()
+    assert org.net_set_active.exists()
+
+    def assert_protected(entity):
+        """
+        helper function to test that an object is currently
+        not deletable
+        """
+        with pytest.raises(ProtectedAction):
+            entity.delete()
+
+
+    # org has ix, net and fac under it, and should not be
+    # deletable
+
+    assert_protected(org)
+
+    # process the exchanges under the org for deletion
+    # and checking their protected status as well
+
+    for ix in org.ix_set.all():
+
+        assert ix.ixlan.ixpfx_set.exists()
+        assert ix.ixlan.netixlan_set.exists()
+
+        # exchange currently has netixlans and prefixes
+        # under it that are active and should not be
+        # deletable
+
+        assert_protected(ix)
+
+        # process the prefixes under the exchange
+
+        for ixpfx in ix.ixlan.ixpfx_set.all():
+
+            # exchange has netixlans under it that fall
+            # into the address space for the prefix so
+            # the prefix is currently not deletable
+
+            assert_protected(ixpfx)
+
+        # process the netixlans under the exchange and
+        # delete them
+
+        for netixlan in ix.ixlan.netixlan_set.all():
+            netixlan.delete()
+
+        # with the netixlans gone, the prefixes can now
+        # be deleted
+
+        for ixpfx in ix.ixlan.ixpfx_set.all():
+            ixpfx.delete()
+
+        # with netixlans gone the exchange can now be
+        # deleted
+
+        ix.delete()
+
+    # org still has active fac and net under it
+    # and should still be protected
+
+    assert_protected(org)
+
+    # process the facilities under the org for deletion
+    # and checking their protected status as well
+
+    for fac in org.fac_set.all():
+        assert fac.ixfac_set.exists()
+        assert fac.netfac_set.exists()
+
+        # fac has active netfac and ixfac objects
+        # under it and should not be deletable
+
+        assert_protected(fac)
+
+        # delete ixfacs
+
+        for ixfac in fac.ixfac_set.all():
+            ixfac.delete()
+
+        # fac has active netfac under it and should
+        # still not be deletable
+
+        assert_protected(fac)
+
+        # delete netfacs
+
+        for netfac in fac.netfac_set.all():
+            netfac.delete()
+
+        # facility can now be deleted
+
+        fac.delete()
+
+    # org still has active net objects under it
+    # and should not be deletable
+
+    assert_protected(org)
+
+    # delete nets
+
+    for net in org.net_set.all():
+        net.delete()
+
+    # org is now deletable
+
+    org.delete()
+    assert org.status == "deleted"
+
+
+
+
+
