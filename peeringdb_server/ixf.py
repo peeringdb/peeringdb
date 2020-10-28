d4b8995bb6aa1ba9249c6cc9ad9e9538708575d0
Stefan Pratter <stefan@20c.com>
2020-10-28 12:13:23 +0000

    fix unhandled deskpro api errors

--- peeringdb_server/ixf.py
+++ peeringdb_server/ixf.py
@@ -1297,7 +1297,12 @@ class Importer:
             ticket.save()
         except Exception as exc:
             ticket.subject = f"[FAILED]{ticket.subject}"
-            ticket.body = f"{ticket.body}\n\n{exc.data}"
+            if hasattr(exc, "data"):
+                # api error returned with validation error data
+                ticket.body = f"{ticket.body}\n\n{exc.data}"
+            else:
+                # api error configuration issue
+                ticket.body = f"{ticket.body}\n\n{exc}"
             ticket.save()
         return ticket
 
