54fe43cd1165958dffac3396e32c46abfab47eda
Elliot Frank <elliot@20c.com>
2021-01-12 13:25:50 -0600

    update tests

--- tests/test_ixf_importer.py
+++ tests/test_ixf_importer.py
@@ -15,8 +15,8 @@ def test_vlan_sanitize(data_ixf_vlan):
 
 def test_connections_match(data_ixf_connections_match):
     importer = ixf.Importer()
-    cxn_match = importer.connections_match(
-        *json.loads(data_ixf_connections_match.input)["connection_list"])
+    connection_list = json.loads(data_ixf_connections_match.input)["connection_list"]
+    cxn_match = importer.connections_match(connection_list[0], connection_list[1])
     assert cxn_match == data_ixf_connections_match.expected
 
 
