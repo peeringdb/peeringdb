be136c42ad0e5f5eec831a0b574d73f57f87c5d9
Elliot Frank <elliot@20c.com>
2021-03-01 17:37:09 -0600

    add migration and regression test for lat long decimal db constraint

--- tests/test_geocode.py
+++ tests/test_geocode.py
@@ -1,15 +1,16 @@
-import pytest
 import json
-import re
 import os
 
-from django.test import Client, TestCase, RequestFactory
-from django.contrib.auth.models import Group, AnonymousUser
-from django.contrib.auth import get_user
-from django.core.management import call_command
+import pytest
 from django.core.exceptions import ValidationError
 
 import peeringdb_server.models as models
+
+
+@pytest.fixture
+def org():
+    org = models.Organization(name="Geocode Org", status="ok")
+    return org
 
 
 @pytest.fixture
@@ -51,12 +52,12 @@ def reverse_parsed():
 
 
 def test_geo_model_defaults(fac):
-    assert fac.geocode_status == False
-    assert fac.geocode_date == None
+    assert fac.geocode_status is False
+    assert fac.geocode_date is None
 
 
 def test_geo_model_geocode_coordinates(fac):
-    assert fac.geocode_coordinates == None
+    assert fac.geocode_coordinates is None
     fac.latitude = 41.876212
     fac.longitude = -87.631453
     assert fac.geocode_coordinates == (41.876212, -87.631453)
@@ -68,7 +69,7 @@ def test_geo_model_geocode_addresss(fac):
 
 def test_geo_model_get_address1(fac):
     data = [{"empty": "empty"}]
-    assert fac.get_address1_from_geocode(data) == None
+    assert fac.get_address1_from_geocode(data) is None
 
     data = load_json("address1_test0")
     assert fac.get_address1_from_geocode(data) == "427 S LaSalle St"
@@ -91,7 +92,8 @@ def test_geo_model_parse_reverse(fac, reverse, reverse_parsed):
     assert fac.parse_reverse_geocode(reverse) == reverse_parsed
 
 
-def test_round_geo_model_lat_long(fac):
+@pytest.mark.django_db
+def test_round_geo_model_lat_long(org, fac):
     # Per issue 865, we remove the DB limit
     # but add rounding to the clean method
 
@@ -99,7 +101,15 @@ def test_round_geo_model_lat_long(fac):
     fac.latitude = 41.123456789
     fac.longitude = -87.987654321
 
+    # Need a saved org to save a facility
+    org.save()
+    fac.org_id = org.id
+    fac.save()
+    assert fac.latitude == 41.123456789
+    assert fac.longitude == -87.987654321
+
     # Lat and long are rounded on clean
     fac.clean()
+    fac.save()
     assert fac.latitude == 41.123457
     assert fac.longitude == -87.987654
