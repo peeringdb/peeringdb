6a449e5c1f72cb59981da33d9293711e19e1acc2
Elliot Frank <elliot@20c.com>
2021-03-08 14:12:28 -0600

    Fix migrations

--- peeringdb_server/migrations/0064_add_namelong_aka.py
+++ peeringdb_server/migrations/0064_add_namelong_aka.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("peeringdb_server", "0064_alter_geo_lat_long_decimals"),
+        ("peeringdb_server", "0063_populate_last_update_fields"),
     ]
 
     operations = [
