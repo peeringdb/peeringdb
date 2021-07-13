3dd72435cae05dfa0953d08af1c4065d0d369e6a
Matt Griswold <grizz@20c.com>
2021-07-13 08:21:43 -0500

    Fix 23 search result order (#1002)
    * fix search results listed in wrong order (#23)
    
    * no longer need to call highlight
    
    Co-authored-by: Stefan Pratter <stefan@20c.com>

--- peeringdb_server/search.py
+++ peeringdb_server/search.py
@@ -108,14 +108,11 @@ def search(term, autocomplete=False):
     result = {tag: [] for tag in categories}
     pk_map = {tag: {} for tag in categories}
 
-    for sq in search_query.highlight()[:limit]:
+    for sq in search_query[:limit]:
         model = sq.model
         tag = model.HandleRef.tag
 
         categorize(sq, result, pk_map)
-
-    for _result in result.values():
-        _result.reverse()
 
     # print("done", time.time() - t0)
 
