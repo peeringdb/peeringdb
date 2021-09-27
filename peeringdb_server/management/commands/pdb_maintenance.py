3c47c08a0369320b33006cd1bee848178309dc3a
Sunshine Buchholz <sunshine@20c.com>
2021-09-27 14:31:47 +0000

    Update pdb_maintenance.py

--- peeringdb_server/management/commands/pdb_maintenance.py
+++ peeringdb_server/management/commands/pdb_maintenance.py
@@ -1,5 +1,5 @@
 """
-Put peeringdb in or out of maintenance mode
+Puts peeringdb in or out of maintenance mode.
 """
 from django.conf import settings
 from django.core.management.base import BaseCommand
