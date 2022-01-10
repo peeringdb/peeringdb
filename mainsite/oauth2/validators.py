1c314cdf1f00e37645e1c4233d895e0a0d0437f1
Carlos Aguado <caguado@infra.structur.es>
2022-01-10 14:34:16 +0100

    Enable OIDC support (#1070) (#1098)
    Enable OIDC and RS256 JWT signing from django-oauth-toolkit with a
    single RSA key. Key rotation is not yet enabled and will be introduced
    on the first rotation.
    
    The JWT signed token contains the previous claims exposed in profile/v1
    endpoint but compatible with the OIDC standard claims.
    
    https://openid.net/specs/openid-connect-core-1_0.html#StandardClaims

---
+++ mainsite/oauth2/validators.py
@@ -0,0 +1,29 @@
+from oauth2_provider.oauth2_validators import OAuth2Validator
+
+from mainsite.oauth2 import claims
+from mainsite.oauth2.scopes import SupportedScopes
+
+
+class OIDCValidator(OAuth2Validator):
+    def get_additional_claims(self):
+        """PeeringDB-specific claims added to the standard claims defined in a JWT token.
+
+        These claims will be omitted if the scope requested does not match any
+        of the scopes the claim is associated with.
+
+        Returns:
+            List[Tuple(str, callable)]: List of claims to be resolved from request details.
+        """
+        return [
+            # Standard claims
+            # https://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
+            ("name", claims.Name([SupportedScopes.PROFILE])),
+            ("given_name", claims.GivenName([SupportedScopes.PROFILE])),
+            ("family_name", claims.FamilyName([SupportedScopes.PROFILE])),
+            ("email", claims.Email([SupportedScopes.EMAIL])),
+            ("email_verified", claims.EmailVerified([SupportedScopes.EMAIL])),
+            # Custom claims
+            ("id", claims.UserId([SupportedScopes.PROFILE])),
+            ("verified_user", claims.UserVerified([SupportedScopes.PROFILE])),
+            ("networks", claims.Networks([SupportedScopes.NETWORKS])),
+        ]
