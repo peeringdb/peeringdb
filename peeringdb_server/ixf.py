4bbe4f1d3df63e26efb68a5ca34564b6df0a64f1
Matt Griswold <grizz@20c.com>
2022-02-10 06:17:26 -0600

    Beta fixes 202202 (#1112)
    * remove survey notifications
    
    * fixing old reference of IXF_IMPORTER_DAYS_UNTIL_TICKET through EnvironmentSettings, this setting is no longer controlled through that and should come straight from settings
    
    * fix pdb_load_data ixlan.status error
    
    Co-authored-by: Stefan Pratter <stefan@20c.com>

--- peeringdb_server/ixf.py
+++ peeringdb_server/ixf.py
@@ -28,7 +28,6 @@ from django.utils.translation import ugettext_lazy as _
 import peeringdb_server.deskpro as deskpro
 from peeringdb_server.models import (
     DeskProTicket,
-    EnvironmentSetting,
     IXFImportEmail,
     IXFMemberData,
     IXLanIXFMemberImportAttempt,
@@ -1742,9 +1741,7 @@ class Importer:
             else:
                 raise
 
-        ticket_days = EnvironmentSetting.get_setting_value(
-            "IXF_IMPORTER_DAYS_UNTIL_TICKET"
-        )
+        ticket_days = settings.IXF_IMPORTER_DAYS_UNTIL_TICKET
 
         template = loader.get_template("email/notify-ixf-consolidated.txt")
 
