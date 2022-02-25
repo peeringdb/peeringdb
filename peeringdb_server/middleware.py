8a641ae07e02af8b23d6734da4856fa4504d2350
David Poarch <dpoarch@20c.com>
2022-02-25 15:13:48 +0000

    To force or not to force www, that is a question #916

--- peeringdb_server/middleware.py
+++ peeringdb_server/middleware.py
@@ -3,6 +3,8 @@ Custom django middleware.
 """
 
 from peeringdb_server.context import current_request
+from django.middleware.common import CommonMiddleware
+from django.conf import settings
 
 
 class CurrentRequestContext:
@@ -19,3 +21,31 @@ class CurrentRequestContext:
     def __call__(self, request):
         with current_request(request):
             return self.get_response(request)
+
+
+class PDBCommonMiddleware(CommonMiddleware):
+    def has_subdomain(self, request):
+        # Check if the request has a subdomain and does not start with www
+        host = request.get_host()
+        if host.startswith("www.") or (len(host.split(".")) > 2):
+            return True
+        return False
+
+    def process_request(self, request):
+        must_prepend = settings.PDB_PREPEND_WWW and not self.has_subdomain(request)
+        redirect_url = (
+            ("%s://www.%s" % (request.scheme, request.get_host()))
+            if must_prepend
+            else ""
+        )
+        # Check if a slash should be appended
+        if self.should_redirect_with_slash(request):
+            path = self.get_full_path_with_slash(request)
+        else:
+            path = request.get_full_path()
+
+        # Return a redirect if necessary
+
+        if redirect_url or path != request.get_full_path():
+            redirect_url += path
+            return self.response_redirect_class(redirect_url)
---
+++ tests/test_middleware.py
@@ -0,0 +1,19 @@
+from peeringdb_server.middleware import PDBCommonMiddleware
+from django.test import RequestFactory, SimpleTestCase, override_settings
+from django.http import HttpResponse
+
+
+def get_response_empty(request):
+    return HttpResponse()
+
+@override_settings(ROOT_URLCONF="middleware.urls")
+class PDBCommonMiddlewareTest(SimpleTestCase):
+
+    rf = RequestFactory()
+
+    @override_settings(PDB_PREPEND_WWW=True)
+    def test_prepend_www(self):
+        request = self.rf.get("/path/")
+        r = PDBCommonMiddleware(get_response_empty).process_request(request)
+        self.assertEqual(r.status_code, 301)
+        self.assertEqual(r.url, "http://www.testserver/path/")
