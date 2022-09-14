05f6fe788e6c5e4d74b5ed88003b5bea4ba46393
Stefan Pratter <stefan@20c.com>
2022-09-14 10:42:05 +0300

    Fix UI bugs to show tooltips, modals, collapsible elements (#1243)
    Co-authored-by: Prithvi Nath Manikonda <manikop@amazon.com>

--- peeringdb_server/static/peeringdb.js
+++ peeringdb_server/static/peeringdb.js
@@ -954,7 +954,7 @@ PeeringDB.IXFPreview = twentyc.cls.define(
       renderTo.find('.ixf-error-counter').empty();
       $.get('/import/ixlan/'+ixlanId+'/ixf/preview', function(result) {
         this.render(result, renderTo);
-      }.bind(this)).error(function(result) {
+      }.bind(this)).fail(function(result) {
         if(result.responseJSON) {
           this.render(result.responseJSON, renderTo);
         } else {
@@ -1076,7 +1076,7 @@ PeeringDB.IXFNetPreview =  twentyc.cls.extend(
       renderTo.find('.ixf-error-counter').empty();
       $.get('/import/net/'+netId+'/ixf/preview', function(result) {
         this.render(result, renderTo);
-      }.bind(this)).error(function(result) {
+      }.bind(this)).fail(function(result) {
         if(result.responseJSON) {
           this.render(result.responseJSON, renderTo);
         } else {
@@ -1155,7 +1155,7 @@ PeeringDB.IXFNetPostmortem =  twentyc.cls.extend(
 
       $.get('/import/net/'+netId+'/ixf/postmortem?limit='+limit, function(result) {
         this.render(result, renderTo);
-      }.bind(this)).error(function(result) {
+      }.bind(this)).fail(function(result) {
         if(result.responseJSON) {
           this.render(result.responseJSON, renderTo);
         } else {
