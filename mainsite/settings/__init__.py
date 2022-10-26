75817dea053382d9a86f6efa44d5f99223be5a76
Stefan Pratter <stefan@20c.com>
2022-10-26 18:42:04 +0300

    Add missing allowed sources for scripts (#1276)
    * add missing allowed sources to content policy
    change redoc cdn to redoc.ly
    
    * set_option for CSP settings
    
    * linting

--- mainsite/settings/__init__.py
+++ mainsite/settings/__init__.py
@@ -673,12 +673,21 @@ set_option("SECURE_HSTS_INCLUDE_SUBDOMAINS", True)
 set_option("SECURE_HSTS_SECONDS", 47304000)
 set_option("SECURE_REFERRER_POLICY", "strict-origin-when-cross-origin")
 
-CSP_DEFAULT_SRC = ("'self'",)
-CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
-CSP_SCRIPT_SRC = ("'self'", "www.google.com", "www.gstatic.com", "'unsafe-inline'")
-CSP_FRAME_SRC = ("'self'", "'unsafe-inline'")
-CSP_FONT_SRC = ("'self'",)
-CSP_IMG_SRC = ("'self'",)
+set_option(
+    "CSP_DEFAULT_SRC",
+    [
+        "'self'",
+    ],
+)
+set_option("CSP_STYLE_SRC", ["'self'", "fonts.googleapis.com", "'unsafe-inline'"])
+set_option(
+    "CSP_SCRIPT_SRC",
+    ["'self'", "www.google.com", "www.gstatic.com", "cdn.redoc.ly", "'unsafe-inline'"],
+)
+set_option("CSP_FRAME_SRC", ["'self'", "www.google.com", "'unsafe-inline'"])
+set_option("CSP_FONT_SRC", ["'self'", "fonts.gstatic.com"])
+set_option("CSP_IMG_SRC", ["'self'", "cdn.redoc.ly", "data:"])
+set_option("CSP_WORKER_SRC", ["'self'", "blob:"])
 
 MIDDLEWARE = (
     "corsheaders.middleware.CorsMiddleware",
