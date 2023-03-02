2bfa2e4e455c0fd3f97358701b1a5c8d294913ff
Matt Griswold <grizz@20c.com>
2023-03-02 03:46:44 +0000

    update compose

--- Ctl/dev/compose.sh
+++ Ctl/dev/compose.sh
@@ -2,5 +2,6 @@
 
 
 COMPOSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_NAME=$(basename $(git rev-parse --show-toplevel))$(basename $COMPOSE_DIR)
 
-docker-compose -f $COMPOSE_DIR/docker-compose.yml $@
+docker-compose -p $PROJECT_NAME -f $COMPOSE_DIR/docker-compose.yml $@
--- Ctl/dev/run.sh
+++ Ctl/dev/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-
 COMPOSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_NAME=$(basename $(git rev-parse --show-toplevel))$(basename $COMPOSE_DIR)
 
-docker-compose -f $COMPOSE_DIR/docker-compose.yml run --rm peeringdb $@
-#docker-compose -f $COMPOSE_DIR/docker-compose.yml exec --rm peeringdb $@
+# check for UID / github actions
+if [[ -n "$UID" ]]; then
+  run_options="-u $UID"
+fi
+
+docker-compose -p $PROJECT_NAME -f $COMPOSE_DIR/docker-compose.yml run $run_options --rm peeringdb $@
