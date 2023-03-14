cf2bb9cd0c768c579760014ae7543d7eebe34f2a
Matt Griswold <grizz@20c.com>
2023-03-14 19:15:34 +0000

    don't change UID for github actions

--- Ctl/dev/run.sh
+++ Ctl/dev/run.sh
@@ -3,9 +3,4 @@
 COMPOSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_NAME=$(basename $(git rev-parse --show-toplevel))$(basename $COMPOSE_DIR)
 
-# check for UID / github actions
-if [[ -n "$UID" ]]; then
-  run_options="-u $UID"
-fi
-
 docker-compose -p $PROJECT_NAME -f $COMPOSE_DIR/docker-compose.yml run $run_options --rm peeringdb $@
