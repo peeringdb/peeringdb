f8a05be53d2c239d048f3c94484ccaa05d5b3613
Matt Griswold <grizz@20c.com>
2023-11-27 17:26:03 +0000

    Cache changes

--- peeringdb_server/management/commands/pdb_load_data.py
+++ peeringdb_server/management/commands/pdb_load_data.py
@@ -7,17 +7,19 @@ from django.core.management.base import BaseCommand
 from django.db.models.signals import pre_save
 from django_peeringdb import models as djpdb_models
 
-import peeringdb._fetch
+import peeringdb.fetch
 from peeringdb import SUPPORTED_BACKENDS, resource
 from peeringdb.client import Client
 from peeringdb.config import ClientSchema
 from peeringdb_server import models as pdb_models
 from peeringdb_server import signals
 
-_fetch_all_latest = peeringdb._fetch.Fetcher.fetch_all_latest
+import logging
+
+Fetcher__entries = peeringdb.fetch.Fetcher.entries
 
 
-def fetch_all_latest(self, R, depth, params={}, since=None):
+def entries(self, tag:str):
     """
     Some foreign keys differ in peeringdb_server from how they
     are exposed in the api, these need to be adjusted before
@@ -34,9 +36,8 @@ def fetch_all_latest(self, R, depth, params={}, since=None):
     Additionally we fix some common validation issues
     """
 
-    result = _fetch_all_latest(self, R, depth, params=params, since=since)
-
-    for row in result[0]:
+    result = Fetcher__entries(self, tag)
+    for row in result:
         for k, v in list(row.items()):
             if k in ["latitude", "longitude"] and v:
                 row[k] = f"{float(v):3.6f}"
@@ -50,7 +51,7 @@ def fetch_all_latest(self, R, depth, params={}, since=None):
     return result
 
 
-peeringdb._fetch.Fetcher.fetch_all_latest = fetch_all_latest
+peeringdb.fetch.Fetcher.entries = entries
 
 
 class Command(BaseCommand):
@@ -68,6 +69,7 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+
         if settings.RELEASE_ENV != "dev" and not settings.TUTORIAL_MODE:
             self.stdout.write(
                 "Command can only be run on dev instances and instances "
@@ -94,20 +96,23 @@ class Command(BaseCommand):
         }
 
         if options.get("no_cache"):
+            self.stdout.write("Disabling sync cache via --no-cache")
             sync_options["cache_url"] = ""
 
         # allow authentication for the sync (set as env vars)
         if settings.PEERINGDB_SYNC_API_KEY:
             sync_options["api_key"] = settings.PEERINGDB_SYNC_API_KEY
+            self.stdout.write(f"Using API key for sync: prefix {settings.PEERINGDB_SYNC_API_KEY[:8]}")
         elif settings.PEERINGDB_SYNC_USERNAME and settings.PEERINGDB_SYNC_PASSWORD:
             sync_options["user"] = settings.PEERINGDB_SYNC_USERNAME
             sync_options["password"] = settings.PEERINGDB_SYNC_PASSWORD
+            self.stdout.write(f"Using username/password for sync: {settings.PEERINGDB_SYNC_USERNAME}")
         else:
-            print(
+            self.stdout.write(
                 "No authentication configured for sync, using anonymous access - this may cause rate limiting issues. Set PEERINGDB_SYNC_API_KEY or PEERINGDB_SYNC_USERNAME and PEERINGDB_SYNC_PASSWORD to configure authentication."
             )
 
-        print("Syncing data from {url}".format(**sync_options))
+        self.stdout.write("Syncing data from {url}".format(**sync_options))
 
         config = {
             "sync": sync_options,
@@ -150,4 +155,10 @@ class Command(BaseCommand):
             settings.ELASTICSEARCH_DSL_AUTO_REFRESH = False
 
         client = Client(config)
-        client.update_all(resource.all_resources(), since=None)
+
+        
+        # if verbosity is > 0 set logging level to show debug messages
+        if options.get("verbosity", 1) > 0:
+            client.fetcher._log.setLevel(logging.DEBUG)
+
+        client.updater.update_all(resource.all_resources(), since=None)
