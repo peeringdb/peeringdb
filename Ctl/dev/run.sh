da698083cf4426ae795fdb2f9269b9884bec6df5
Stefan Pratter <stefan@20c.com>
2024-08-13 18:48:44 +0000

    docker-compose to docker compose

--- Ctl/dev/run.sh
+++ Ctl/dev/run.sh
@@ -3,4 +3,4 @@
 COMPOSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_NAME=$(basename $(git rev-parse --show-toplevel))$(basename $COMPOSE_DIR)
 
-docker-compose -p $PROJECT_NAME -f $COMPOSE_DIR/docker-compose.yml run $run_options --rm peeringdb $@
+docker compose -p $PROJECT_NAME -f $COMPOSE_DIR/docker-compose.yml run $run_options --rm peeringdb $@
