15e826ae862b762c7dda4e9f51e37e8872dc185b
Stefan Pratter <stefan@20c.com>
2025-11-20 09:03:26 +0000

    fix "checkmark" static file check failure

--- mainsite/settings/__init__.py
+++ mainsite/settings/__init__.py
@@ -884,7 +884,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
